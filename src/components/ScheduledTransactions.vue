eb575324dfb03031aa77355179559c7b044c407b
groundskeeper34 <ckuhl45@gmail.com>
2022-10-07 13:46:36 -0500

    filter

--- src/components/ScheduledTransactions.vue
+++ src/components/ScheduledTransactions.vue
@@ -14,7 +14,7 @@
     </thead>
     <tbody>
       <tr v-for="scheduled_transaction in scheduledTransactions" v-bind:key="scheduled_transaction.id">
-        <template v-if="scheduled_transaction.date_next <= '2022-11-01'">
+        <template v-if="scheduled_transaction.date_next <= '2022-12-01'">
         <td>{{scheduled_transaction.account_name}}</td>
         <td>{{scheduled_transaction.date_next}}</td>
         <td>{{scheduled_transaction.payee_name}}</td>
