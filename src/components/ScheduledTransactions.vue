f1cd90036f95170fa329bab7fb73ecb3c8404c12
groundskeeper34 <ckuhl45@gmail.com>
2022-10-07 13:44:43 -0500

    filter

--- src/components/ScheduledTransactions.vue
+++ src/components/ScheduledTransactions.vue
@@ -14,7 +14,7 @@
     </thead>
     <tbody>
       <tr v-for="scheduled_transaction in scheduledTransactions" v-bind:key="scheduled_transaction.id">
-        <template v-if="scheduled_transaction.date_next == '2022-11-01'">
+        <template v-if="scheduled_transaction.date_next <= '2022-11-01'">
         <td>{{scheduled_transaction.account_name}}</td>
         <td>{{scheduled_transaction.date_next}}</td>
         <td>{{scheduled_transaction.payee_name}}</td>
@@ -22,7 +22,7 @@
         <td>{{scheduled_transaction.memo}}</td>
         <td>{{convertMilliUnitsToCurrencyAmount(scheduled_transaction.amount).toFixed(2)}}</td>
       </template>
-      
+
       </tr>
     </tbody>
     </table>
